52b09e0f3b67aba4b63b03fee4534509e6ccf410
Juan Azambuja <juan@mimiquate.com>
2017-01-31 21:00:14 -0300

    use custom container and latest version of telling stories

--- tests/pages/folder.js
+++ tests/pages/folder.js
@@ -8,14 +8,16 @@ import {
 
 export default create({
   visit: visitable('/inbox'),
+  testContainer: '#test-root',
+
   emails: collection({
     itemScope: 'table tr',
     item: {
-      checked: clickable('[type=checkbox]'),
-      star: clickable('td', { at: 1}),
-      starred: hasClass('inbox-started', 'i', { scope: 'td:eq(1)'})
+      checked: clickable('[type=checkbox]', { testContainer: '#test-root'}),
+      star: clickable('td', { at: 1, testContainer: '#test-root'}),
+      starred: hasClass('inbox-started', 'i', { scope: 'td:eq(1)', testContainer: '#test-root'})
     }
   }),
 
-  trash: clickable('.mail-option .fa-trash')
+  trash: clickable('.mail-option .fa-trash', { testContainer: '#test-root' })
 });
